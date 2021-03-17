54a9e717996c076e569fefa6b5eef2b4da0a13d2
Nicoll Oliver <nicolldon96@gmail.com>
2021-03-16 23:14:02 -0400

    commit tree 364992806cf8bc6de9ed1143c57764aff75423f9

--- app.rb
+++ app.rb
@@ -9,6 +9,11 @@ class App < Sinatra::Base
   post '/' do
     text_from_user = params[:user_text]
 
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+
     erb :results
   end
+  # post '/' do @analyzedtext = TextAnalyzer.new(params[:usertext])
+
+  #   erb :results
 end
